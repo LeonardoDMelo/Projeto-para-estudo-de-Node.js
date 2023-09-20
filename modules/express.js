a5aee2bf95ce39265a964eec53596640a8a3e9e0
LeonardoDMelo <leonardodavimelo@live.com>
2023-09-20 17:42:34 -0300

    Integração com Postman

--- modules/express.js
+++ modules/express.js
@@ -10,18 +10,26 @@ app.get("/home", (req, res) => {
   res.status(200).send("<h1>Hello World</h1>");
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Leonardo",
-      email: "leonardo@gmail.com",
-    },
-    {
-      name: "Lidiane",
-      email: "lidiane@gmail.com",
-    },
-  ];
-  res.status(200).json(users);
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 app.post("/users", async (req, res) => {
@@ -29,6 +37,32 @@ app.post("/users", async (req, res) => {
     const user = await UserModel.create(req.body);
 
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
